3c6aa7cf1d191434564129f69da5e6abbd0b9c72
ngactive99 <ngactive99@gmail.com>
2022-10-07 17:05:57 +0530

    added array, spread operator, map, ifelse, loops, switch case

--- Javascript_and_Html/index.js
+++ Javascript_and_Html/index.js
@@ -18,38 +18,103 @@ console.log(marks);
   }
   add();
   function multiply() {
-    const c = a * b ;
+    const c = a * b;
     console.log("multiplication value is " + c);
   }
   multiply();
   function division() {
-    const c = a / b ;
+    const c = a / b;
     console.log("division value is " + c);
   }
   division();
   function mod() {
-    const c = a % b ;
+    const c = a % b;
     console.log("mod value is " + c);
   }
   mod();
-  console.log("post fix increment "+ num++ ) // post fix increment 
+  console.log("post fix increment " + num++); // post fix increment
   console.log(num);
-  console.log("post fix decrement "+  num--); // post fix decrement
+  console.log("post fix decrement " + num--); // post fix decrement
   console.log(num);
-  console.log("pre fix increment "+ ++num ); // pre fix increment 
+  console.log("pre fix increment " + ++num); // pre fix increment
   console.log(num);
-  console.log("pre fix decrement "+ --num ); // pre fix decrement 
+  console.log("pre fix decrement " + --num); // pre fix decrement
   console.log(num);
 
   let hello;
- // alert(!hello); // not -> logical operator use* does not*
+  // alert(!hello); // not -> logical operator use* does not*
 
- // and operator 
-  alert(5 && 10 && 80 ); // and operator gives us the bigger number 
+  // and operator
+  // alert(5 && 10 && 80 ); // and operator gives us the bigger number
 
-// or opreator
-console.log(10 || 50 || 100); // or opreator gives us the value of the smallest data 
+  // or opreator
+  console.log(10 || 50 || 100); // or opreator gives us the value of the smallest data
 
+  // array
+
+  const names = ["nishant", "raj", "sam"];
+  names[3] = "Ram";
+  names[0] = "vijay";
+  console.log(names);
+
+  const food = [];
+  food[0] = "pasta";
+  food[1] = "pizza";
+  food[2] = "Curd rice";
+
+  const menu = [...food, "biryani", "dosa", "chicken curry", "fish"];
+  console.log(menu);
+
+  const marks = [12, 32, 123, 445, 3121, 42211];
+  let maxMark = Math.max(...marks);
+  console.log(maxMark);
+
+  const students = new Map([
+    [29, 80],
+    ["Rishav", true],
+    ["Rohit", 31],
+    ["Vijay", 32],
+  ]);
+
+  students.set("John", 33); // set method allows us to add a key- value paid after setting the data
+
+  console.log(students);
+  console.log(students.get("Rohit")); // get allows to retrieve a data
+
+  for (let index = 0; index < menu.length; index++) {
+    console.log(menu[index]);
+  }
+
+  if (menu.length > 10) {
+    console.log("we have more than 10 food items ");
+  }
+  if (menu.length == 10) {
+    console.log("we have 10 food items ");
+  } else {
+    console.log("we have less than 10 food items ");
+  }
+
+  switch(menu.length) {
+    case 0 : console.log("no items in menu");
+    break;
+    case 1 : console.log("1 items in menu");
+    break;
+    case 2 : console.log("2 items in menu");
+    break;
+    case 3 : console.log("3 items in menu");
+    break;
+    case 4 : console.log("4 items in menu");
+    break;
+    case 5 : console.log("5 items in menu");
+    break;
+    case 6 : console.log("6 items in menu");
+    break;
+    case 7 : console.log("7 items in menu");
+    break;
+    case 8 : console.log("8 items in menu");
+    break;
+    default: console.log("more than 8 items ");
+  }
 
 
 
