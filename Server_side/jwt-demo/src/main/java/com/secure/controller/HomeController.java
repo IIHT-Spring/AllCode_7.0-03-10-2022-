d1f8f559e8268c61872b223fc6b0a0b56b03db00
ngactive99 <ngactive99@gmail.com>
2022-10-21 15:11:03 +0530

    swagger a[[ and added our angular appplication, did clean up of the app

--- Server_side/jwt-demo/src/main/java/com/secure/controller/HomeController.java
+++ Server_side/jwt-demo/src/main/java/com/secure/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.secure.controller;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	
 	@GetMapping("/")
-	public String home() {
-		return "hello everyone";
-		
+	public String home(Principal principal) {
+		return "hello and welcome Mr " + principal.getName();
+		// username = user
 	}
 
 }
