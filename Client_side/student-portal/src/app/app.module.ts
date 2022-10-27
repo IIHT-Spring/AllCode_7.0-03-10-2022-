c352aba7c4eb6a2196e960626774c50c84ed8d2c
ngactive99 <ngactive99@gmail.com>
2022-10-27 14:15:22 +0530

    added navbar

--- Client_side/student-portal/src/app/app.module.ts
+++ Client_side/student-portal/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { StudentRegComponent } from './Components/student-reg/student-reg.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    StudentRegComponent
+    StudentRegComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
