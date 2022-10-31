e95e97837ff4853be9c027939b7dd5f1f5c1365d
ngactive99 <ngactive99@gmail.com>
2022-10-31 15:55:09 +0530

    added todo enum and todo actions

---
+++ Client_side/state-management-app/src/app/actions/todo.action.ts
@@ -0,0 +1,13 @@
+import { Action } from "@ngrx/store";
+import { TodoActionType } from "../shared/todo-actions-types.enum";
+
+export class ActionParent implements Action {
+    type: any;
+    payload:any;
+}
+
+
+export class TodoAdd implements ActionParent {
+    type: TodoActionType.Add;
+    constructor(public payload: any) {}
+}
