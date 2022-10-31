02f89a9cf8cd262bbbc40a7f390b7125c6fdfc64
ngactive99 <ngactive99@gmail.com>
2022-10-31 17:28:33 +0530

    todo funcationality implemented (not running with a bug )

--- Client_side/state-management-app/src/app/reducers/todo.reducer.ts
+++ Client_side/state-management-app/src/app/reducers/todo.reducer.ts
@@ -1,6 +1,7 @@
 
 import { ActionParent } from "../actions/todo.action";
-import {Todo} from "../models/Todo";;
+import {Todo} from "../models/Todo";import { TodoActionType } from "../shared/todo-actions-types.enum";
+;
 
 
 const initialState: Todo[] = [
@@ -11,6 +12,8 @@ const initialState: Todo[] = [
 
 export function TodoReducer(state = initialState, action: ActionParent){
     switch(action.type) {
-        default:return state;
+        case TodoActionType.Add:
+        return [...state, action.payload]
+        default: return state;
     }
 }
